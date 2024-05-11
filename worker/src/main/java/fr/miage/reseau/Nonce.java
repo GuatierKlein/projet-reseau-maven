52cb352dd69ba77661304b363b8b44dcb4810439
Guatier.Klein <klein_gautier@yahoo.Fr>
2024-05-11 15:46:19 +0200

    passage de check des hexstring à concat direct des bytes

--- worker/src/main/java/fr/miage/reseau/Nonce.java
+++ worker/src/main/java/fr/miage/reseau/Nonce.java
@@ -1,5 +1,8 @@
 package fr.miage.reseau;
 
+import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+
 public class Nonce {
     private long _value;
     private int _clientIndex;
@@ -13,8 +16,40 @@ public class Nonce {
         _value += _clientIndex;
     }
 
+    public String toHexString() {
+        return Long.toHexString(_value);
+    }
+
     public String toString() {
-        return Long.toHexString(_value).toUpperCase();
+        try {
+            return new String(getBytes(), "UTF-8");
+        } catch(UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public byte[] getBytes() {
+        ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+        buffer.putLong(_value);
+        return trimByteArray(buffer.array());
+    }
+
+    private byte[] trimByteArray(byte[] array) {
+        int i = 0;
+        while (i < array.length && array[i] == 0) {
+            i++;
+        }
+
+        if(i == array.length)
+            return new byte[] {0};
+
+        int trimmedLength = array.length - i;
+        byte[] trimmedArray = new byte[array.length - i];
+        System.arraycopy(array, i, trimmedArray, 0, trimmedLength);
+
+        return trimmedArray;
     }
 }
+
 
