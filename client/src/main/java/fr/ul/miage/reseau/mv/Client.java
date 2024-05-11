ca185051ec0d90a259f24abc9f47bb154f4b2825
MullerTheodore <theodore.muller31@gmail.com>
2024-05-11 14:01:38 +0200

    Ajout partie thméodule

--- client/src/main/java/fr/ul/miage/reseau/mv/Client.java
+++ client/src/main/java/fr/ul/miage/reseau/mv/Client.java
@@ -5,70 +5,23 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client{
 
     private static final String SERVER_ADDRESS = "localhost";
     private static final int PORT = 1337;
     
-    private static boolean isMining = true;
-    private static String pwd ="test";
-    private static String actualNonce ="1";
-    private static boolean sendSolution = false;
+    // private static boolean isMining = true;
+    // private static String pwd ="test";
+    // private static String actualNonce ="1";
+    // private static boolean sendSolution = false;
 	
     //////////////////////////////////MAIN///////////////////////////////////////////////////////////
     
-    // main avec saisie automatique
+    // main avec input de l'utilisateur
     
     public static void main(String[] args) {
-
-        final String SERVER_ADDRESS = "localhost";
-        final int PORT = 1337;
-
-        // Connexion au serveur
-        try (Socket socket = new Socket(SERVER_ADDRESS, PORT);
-             PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
-             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-
-        	 String serverMessage;
-             while ((serverMessage = reader.readLine()) != null) {
-                System.out.println("Instruction reçue du serveur : " + serverMessage); // Afficher l'instruction dans le terminal
-                if (serverMessage.equals("WHO_ARE_YOU_?")) {
-                    // Envoyer des messages au serveur
-                    writer.println(respondToWhoAreYou());
-                }
-                if (serverMessage.equals("GIMME_PASSWORD")){
-                    writer.println(providePassword(pwd)); //Récupérer le password aléatoire envoyé par le serveur (cf Marcus)
-                    // Envoyer mdp au serveur
-                }
-                if (serverMessage.equals("HELLO_YOU") || serverMessage.equals("SOLVED") || serverMessage.equals("CANCELLED")){
-                    writer.println(indicateReady());
-                    // Indiquer au serveur qu'on est prêt à commencer une tâche ou pour la suivante si celle en cours vient d'être achevée ou annulée
-                }
-                if (serverMessage.equals("PROGRESS")){
-                    writer.println(indicateWorkerStatus(isMining, actualNonce)); //Récupérer le nonce du worker (cf Gautier)
-                    //test envoi solution
-                    sendSolution = true;
-                    // Indiquer au serveur le statut du worker
-                }
-                if (sendSolution) {
-                    //Envoyer la solution
-                    writer.println(provideSolution("0c4f12188163dae848bd233757f3b0966972dd9efcaa54af4de92dfceb2c755e","1"));
-                    sendSolution = false; // Réinitialisation du booléen
-                }
-            }
-
-            //Deconnexion du serveur
-            socket.close();
-
-        } catch (IOException e) {
-            System.err.println("Erreur lors de la connexion au serveur : " + e.getMessage());
-        }
-    }
-    
-// main avec input de l'utilisateur
-    
-    /*public static void main(String[] args) {
 
         final String SERVER_ADDRESS = "localhost";
         final int PORT = 1337;
@@ -100,6 +53,52 @@ public class Client{
                 }
                 if (sendSolution) {
                     writer.println(scanner.nextLine());
+                    sendSolution = false; // Réinitialisation du booléen
+                }
+            }
+
+            //Deconnexion du serveur
+            socket.close();
+
+        } catch (IOException e) {
+            System.err.println("Erreur lors de la connexion au serveur : " + e.getMessage());
+        }
+    }
+
+
+    // main avec saisie automatique
+    
+    /*public static void main(String[] args) {
+
+        // Connexion au serveur
+        try (Socket socket = new Socket(SERVER_ADDRESS, PORT);
+             PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+
+        	 String serverMessage;
+             while ((serverMessage = reader.readLine()) != null) {
+                System.out.println("Instruction reçue du serveur : " + serverMessage); // Afficher les instructions du serveur dans le terminal
+                if (serverMessage.equals("WHO_ARE_YOU_?")) {
+                    // Envoyer des messages au serveur
+                    writer.println(respondToWhoAreYou());
+                }
+                if (serverMessage.equals("GIMME_PASSWORD")){
+                    writer.println(providePassword(pwd)); //Récupérer le password aléatoire envoyé par le serveur (cf Marcus)
+                    // Envoyer mdp au serveur
+                }
+                if (serverMessage.equals("HELLO_YOU") || serverMessage.equals("SOLVED") || serverMessage.equals("CANCELLED")){
+                    writer.println(indicateReady());
+                    // Indiquer au serveur qu'on est prêt à commencer une tâche ou pour la suivante si celle en cours vient d'être achevée ou annulée
+                }
+                if (serverMessage.equals("PROGRESS")){
+                    writer.println(indicateWorkerStatus(isMining, actualNonce)); //Récupérer le nonce du worker (cf Gautier)
+                    //test envoi solution
+                    sendSolution = true;
+                    // Indiquer au serveur le statut du worker
+                }
+                if (sendSolution) {
+                    //Envoyer la solution
+                    writer.println(provideSolution("0c4f12188163dae848bd233757f3b0966972dd9efcaa54af4de92dfceb2c755e","1"));
                     sendSolution = false; // Réinitialisation du booléen
                 }
             }
