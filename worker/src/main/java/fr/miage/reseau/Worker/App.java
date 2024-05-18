6c6df4e9f7ad6f8b7c6ed39aa0b8bda259b5d371
Guatier.Klein <klein_gautier@yahoo.Fr>
2024-05-18 19:00:21 +0200

    worker contuniously listening

--- worker/src/main/java/fr/miage/reseau/Worker/App.java
+++ worker/src/main/java/fr/miage/reseau/Worker/App.java
@@ -8,10 +8,7 @@ public class App {
         }
 
         System.out.println("Demarrage du client...");
-    
-        Worker worker = new Worker(args[0], args[1], Integer.parseInt(args[2]));
-        Thread readerThread = new Thread(worker);
-        readerThread.start(); 
-        System.out.println("Client demarre");
+        new Worker(args[0], args[1], Integer.parseInt(args[2])).run();;
+        System.out.println("CLient arrete");
     }
 }
--- worker/src/main/java/fr/miage/reseau/Worker/Worker.java
+++ worker/src/main/java/fr/miage/reseau/Worker/Worker.java
@@ -2,6 +2,7 @@ package fr.miage.reseau.Worker;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
@@ -19,21 +20,36 @@ public class Worker implements Runnable {
     @Override
     public void run() {
         // System.out.println("En Attente du serveur...");
-        try {              
+        try {     
+                    
             Socket clientSocket = new Socket(serverAddress, port);
             final DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
             final BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             interpeter.setOutToServer(outToServer);
-            Message message = new Message(inFromServer.readLine());
 
-            for (MessageLine line : message.getLines()) {
+            //reader Thread
+              Thread readerThread = new Thread(() -> {
                 try {
-                    interpeter.execute(line);
-                } catch (Exception e) {
-                    System.out.println(e.getMessage());
+                    String response;
+                    while ((response = inFromServer.readLine()) != null) {
+                        System.out.println("Réponse du serveur: " + response);
+                        Message message = new Message(response);
+                        System.out.println("lala"); 
+                        for (MessageLine line : message.getLines()) {
+                            try {
+                                interpeter.execute(line);
+                            } catch (Exception e) {
+                                System.out.println(e.getMessage());
+                            }
+                        }
                 }
-            }
-            clientSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+
+            readerThread.start();
+            // clientSocket.close();
         } catch (Exception e) {
             System.out.println("Connection echoue");
             e.printStackTrace();
